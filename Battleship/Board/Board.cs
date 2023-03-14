e512c3bd9a44b3a41dcb192119d6de6680e34c2b
Luis Guilherme <luislebert01@outlook.com>
2023-03-14 08:57:15 -0300

    Transformando em OO

---
+++ Battleship/Board/Board.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.Board
+{
+    internal class Board
+    {
+        public Piece[,] board;
+
+        public Board(int lines, int columns)
+        {
+            board = new Piece[lines, columns];
+        }
+
+        public void PrintBoard ()
+        {
+            Console.Write("   | A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T |");
+            Console.Write("\n------------------------------------------------------------------------------------");
+            for (int line = 0; line < board.GetLength(0); line++)
+            {
+                Console.Write("\n" + (line + 1).ToString("D2") + " | ");
+                for (int column = 0; column < board.GetLength(1); column++)
+                {
+                    if (board[line, column] == null)
+                        Console.Write("  | ");
+                    else
+                        Console.Write(board[line, column] + " | ");
+                }
+
+                Console.Write("\n------------------------------------------------------------------------------------");
+            }
+        }
+    }
+}
--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -1,26 +1,14 @@
-﻿internal class Program
+﻿using Battleship.Board;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        char[,] board = new char[20,20];
+        Board board = new (20,20);
         board[0, 0] = 'X';
         board[9, 9] = 'O';
 
         // impressao tabuleiro
-        Console.Write("   | A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T |");
-        Console.Write("\n------------------------------------------------------------------------------------");
-        for (int line = 0; line < 20; line++)
-        {
-            Console.Write("\n" + (line + 1).ToString("D2") + " | ");
-            for (int column = 0; column < 20; column++)
-            {
-                if (board[line, column] == '\0')
-                    Console.Write("  | ");
-                else
-                    Console.Write(board[line, column] + " | ");
-            }
-
-            Console.Write("\n------------------------------------------------------------------------------------");
-        } 
+       
     }
 }
